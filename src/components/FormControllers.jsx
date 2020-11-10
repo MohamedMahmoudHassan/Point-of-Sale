5ca5451db2ffd6c56a1e00534f91774bdb6e96bb
Mohamed <mohamedmahmoudalihassan@gmail.com>
2020-11-10 11:39:46 +0200

    Form cancel button navigation

--- src/components/FormControllers.jsx
+++ src/components/FormControllers.jsx
@@ -1,12 +1,19 @@
 import React from "react";
 import { Button, Row, Col } from "antd";
 import colors from "../config/colors";
+import { useHistory } from "react-router-dom";
 
 export default function FormControllers() {
+  const history = useHistory();
+
+  const handleNav = () => {
+    history.goBack();
+  };
+
   return (
     <Row justify="end">
       <Col>
-        <Button type="primary" htmlType="submit">
+        <Button type="primary" htmlType="submit" onClick={handleNav}>
           Save
         </Button>
       </Col>
@@ -14,6 +21,7 @@ export default function FormControllers() {
         <Button
           htmlType="button"
           style={{ color: colors.primary, borderColor: colors.primary, marginLeft: "8px" }}
+          onClick={handleNav}
         >
           Cancel
         </Button>

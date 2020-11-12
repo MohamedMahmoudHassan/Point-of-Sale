ea7b207a2d6963decbfcc61b0efb4c5778556f4f
Mohamed <mohamedmahmoudalihassan@gmail.com>
2020-11-12 14:22:22 +0200

    Extract CategoryForm comp

---
+++ src/components/CategoryForm.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Form, Input } from "antd";
+import { useTranslation } from "react-i18next";
+import { useHistory } from "react-router-dom";
+import FormButtons from "./FormButtons";
+
+export default function CategoryForm({ data, onFinish }) {
+  const { t } = useTranslation();
+
+  return (
+    <Form name="newCategory" onFinish={onFinish} initialValues={data}>
+      <Form.Item
+        label={t("items.categoriesList.name")}
+        name="label"
+        rules={[{ required: true, message: "Input valid category name" }]}
+      >
+        <Input />
+      </Form.Item>
+      <Form.Item>
+        <FormButtons />
+      </Form.Item>
+    </Form>
+  );
+}
--- src/components/NewCategory.jsx
+++ src/components/NewCategory.jsx
@@ -1,13 +1,12 @@
 import React from "react";
-import { Form, Input } from "antd";
+import CategoryForm from "./CategoryForm";
 import { useTranslation } from "react-i18next";
-import { useHistory } from "react-router-dom";
-import FormButtons from "./FormButtons";
 import categoriesAPI from "../api/categories";
+import { useHistory } from "react-router-dom";
 
-export default function NewCategory() {
-  const history = useHistory();
+export default function NewCategory({ data }) {
   const { t } = useTranslation();
+  const history = useHistory();
 
   const onFinish = async values => {
     values.label = { en: values.label };
@@ -17,19 +16,8 @@ export default function NewCategory() {
 
   return (
     <div className="form-container">
-      <Form name="newCategory" onFinish={onFinish}>
-        <h2>{t("items.categoriesList.addCategory")}</h2>
-        <Form.Item
-          label={t("items.categoriesList.name")}
-          name="label"
-          rules={[{ required: true, message: "Input valid category name" }]}
-        >
-          <Input />
-        </Form.Item>
-        <Form.Item>
-          <FormButtons />
-        </Form.Item>
-      </Form>
+      <h2>{t("items.categoriesList.addCategory")}</h2>
+      <CategoryForm onFinish={onFinish} />
     </div>
   );
 }

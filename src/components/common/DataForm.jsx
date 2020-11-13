37dfb6a6b09823002462fb700cb365e766837951
Mohamed <mohamedmahmoudalihassan@gmail.com>
2020-11-13 16:52:29 +0200

    Extract DataForm component

---
+++ src/components/common/DataForm.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Form } from "antd";
+import FormButtons from "./FormButtons";
+
+export default function DataForm({ title, formItems, onFinish, data }) {
+  const layout = { labelCol: { span: 5 } };
+
+  return (
+    <Form {...layout} onFinish={onFinish} initialValues={data}>
+      <h2>{title}</h2>
+      {formItems.map(item => (
+        <Form.Item label={item.label} name={item.name} rules={item.rules}>
+          <item.Component />
+        </Form.Item>
+      ))}
+      <Form.Item>
+        <FormButtons />
+      </Form.Item>
+    </Form>
+  );
+}

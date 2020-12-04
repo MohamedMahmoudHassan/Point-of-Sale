69fe4e937f25eb27b5eced76ae2270095f9f93be
Mohamed <mohamedmahmoudalihassan@gmail.com>
2020-12-04 22:07:51 +0200

    Release

--- src/config/api.js
+++ src/config/api.js
@@ -1,5 +1,5 @@
 export default {
-  apiHost: "http://192.168.1.4:5000/api",
-  apiHostStatic: "http://192.168.1.4:5000/api/static",
+  apiHost: "https://point-of-sale-1024-backend.herokuapp.com/api",
+  apiHostStatic: "https://point-of-sale-1024-backend.herokuapp.com/api/static",
   altImage: `https://dummyimage.com/400x600/b5b5b5/fff&text=Food`
 };

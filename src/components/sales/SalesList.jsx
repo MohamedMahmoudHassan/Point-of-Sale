797faeea4a67c2c0edf658c6c1637726154cdc34
Mohamed <mohamedmahmoudalihassan@gmail.com>
2020-12-04 19:52:22 +0200

    Add SalesList component

---
+++ src/components/sales/SalesList.jsx
@@ -0,0 +1,47 @@
+import React, { useContext } from "react";
+import DataTable from "./../common/DataTable";
+import salesAPI from "../../api/sales";
+import { useTranslation } from "react-i18next";
+import DataContext from "./../context/dataContext";
+
+export default function SalesList() {
+  const { t } = useTranslation();
+  const { store } = useContext(DataContext);
+
+  const columns = [
+    {
+      title: t("sales.status"),
+      dataIndex: "status"
+    },
+    {
+      title: t("sales.noOfItems"),
+      dataIndex: "items",
+      render: items => items.length,
+      sorter: { compare: (a, b) => a.items.length - b.items.length }
+    },
+    {
+      title: t("sales.total"),
+      dataIndex: "total",
+      sorter: { compare: (a, b) => a.total - b.total }
+    },
+    {
+      title: t("sales.lastUpdate"),
+      dataIndex: "lastUpdateOn",
+      render: lastUpdateOn => new Date(lastUpdateOn).toUTCString().slice(5, -4),
+      sorter: { compare: (a, b) => a.lastUpdateOn - b.lastUpdateOn }
+    }
+  ];
+
+  const getData = async () => await salesAPI.getSales(store);
+
+  return (
+    <DataTable
+      newButtonTitle={t("sales.newSale")}
+      columns={columns}
+      getData={getData}
+      deleteData={() => {}}
+      navTo="/sales/summary"
+      clickable={true}
+    />
+  );
+}
--- src/components/sales/SalesNavigation.jsx
+++ src/components/sales/SalesNavigation.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
 import NewSale from "./NewSale";
-import SaleSummary from './SaleSummary';
+import SaleSummary from "./SaleSummary";
+import SalesList from "./SalesList";
 
 export default function SalesNavigation() {
   return (
     <Switch>
       <Route path="/sales/summary/:id" component={SaleSummary} />
       <Route path="/sales/new" component={NewSale} />
+      <Route path="/sales" component={SalesList} />
     </Switch>
   );
 }
